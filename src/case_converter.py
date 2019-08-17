c02b2ec9ce3d27cc883dd443dece72ea15de640c
Daniel Fernandes <danielpsf@gmail.com>
2019-08-17 14:46:07 -0300

    Fix pip8 warning

--- src/case_converter.py
+++ src/case_converter.py
@@ -1,4 +1,4 @@
-def convert_from_snake_to_camel_case(snake_case: str) ->str:
+def convert_from_snake_to_camel_case(snake_case: str) -> str:
     words_list = snake_case.split("_")
     return words_list[0] + "".join([word.capitalize() for word in words_list[-1:]])
 
