e1d1a6909a66b649baa67176f846e58a2285619b
Daniel Fernandes <danielpsf@gmail.com>
2019-08-17 14:44:56 -0300

    Add 1st green phase to #1 for snake_case to kebab-case

--- src/case_converter.py
+++ src/case_converter.py
@@ -1,3 +1,8 @@
 def convert_from_snake_to_camel_case(snake_case: str) ->str:
     words_list = snake_case.split("_")
     return words_list[0] + "".join([word.capitalize() for word in words_list[-1:]])
+
+
+def convert_from_snake_to_kebab_case(snake_case: str) -> str:
+    words_list = snake_case.split("_")
+    return "-".join([word for word in words_list])
--- test/test_case_converter.py
+++ test/test_case_converter.py
@@ -3,7 +3,7 @@ import unittest
 from parameterized import parameterized
 from sure import expect
 
-from src.case_converter import convert_from_snake_to_camel_case
+from src.case_converter import convert_from_snake_to_camel_case, convert_from_snake_to_kebab_case
 
 
 class CaseConverterTestCase(unittest.TestCase):
@@ -27,6 +27,26 @@ class CaseConverterTestCase(unittest.TestCase):
     ])
     def test_convert_from_snake_case_to_camel_case(self, name, snake_case, camel_case):
         expect(convert_from_snake_to_camel_case(snake_case)).equal_to(camel_case)
+
+    @parameterized.expand([
+        (
+                'Should convert mashed_potato to mashed-potato',
+                'mashed_potato',
+                'mashed-potato',
+        ),
+        (
+                'Should convert fried_potato to fried-potato',
+                'fried_potato',
+                'fried-potato',
+        ),
+        (
+                'Should convert backed_potato to baked-potato',
+                'baked_potato',
+                'baked-potato',
+        )
+    ])
+    def test_convert_from_snake_case_to_kebab_case(self, name, snake_case, kebab_case):
+        expect(convert_from_snake_to_kebab_case(snake_case)).equal_to(kebab_case)
 
 
 if __name__ == '__main__':
