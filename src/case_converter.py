ca41af2f2f110570236a400b74455703ad8b5049
Daniel Fernandes <danielpsf@gmail.com>
2019-08-17 22:53:38 -0300

    Add helper function to cover #1 from camelCase to snake_case

--- src/case_converter.py
+++ src/case_converter.py
@@ -6,3 +6,14 @@ def convert_from_snake_to_camel_case(snake_case: str) -> str:
 def convert_from_snake_to_kebab_case(snake_case: str) -> str:
     words_list = snake_case.split("_")
     return "-".join([word for word in words_list])
+
+
+def convert_from_camel_case_to_snake_case(camel_case: str) -> str:
+    snake_case = ""
+    for i in range(len(camel_case)):
+        if camel_case[i].isupper():
+            snake_case += "_" + camel_case[i:i+1].lower()
+        else:
+            snake_case += camel_case[i]
+
+    return snake_case
--- test/test_case_converter.py
+++ test/test_case_converter.py
@@ -3,7 +3,8 @@ import unittest
 from parameterized import parameterized
 from sure import expect
 
-from src.case_converter import convert_from_snake_to_camel_case, convert_from_snake_to_kebab_case
+from src.case_converter import convert_from_snake_to_camel_case, convert_from_snake_to_kebab_case, \
+    convert_from_camel_case_to_snake_case
 
 
 class CaseConverterTestCase(unittest.TestCase):
@@ -47,6 +48,31 @@ class CaseConverterTestCase(unittest.TestCase):
     ])
     def test_convert_from_snake_case_to_kebab_case(self, name, snake_case, kebab_case):
         expect(convert_from_snake_to_kebab_case(snake_case)).equal_to(kebab_case)
+
+    @parameterized.expand([
+        (
+                'Should convert mashedPotato to mashed_potato',
+                'mashedPotato',
+                'mashed_potato',
+        ),
+        (
+                'Should convert friedPotato to fried_potato',
+                'friedPotato',
+                'fried_potato',
+        ),
+        (
+                'Should convert backedPotato to baked_potato',
+                'bakedPotato',
+                'baked_potato',
+        ),
+        (
+                'Should convert backedPotatoWithCreamCheese to baked_potato_with_cream_cheese',
+                'bakedPotatoWithCreamCheese',
+                'baked_potato_with_cream_cheese',
+        )
+    ])
+    def test_convert_from_camel_case_to_snake_case(self, name, camel_case, snake_case):
+        expect(convert_from_camel_case_to_snake_case(camel_case)).equal_to(snake_case)
 
 
 if __name__ == '__main__':
